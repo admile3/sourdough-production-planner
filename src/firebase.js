e0b11bb19d1173652ce548da4050f623cc98aaec
admile3 <adamd.miles@gmail.com>
2026-05-14 13:59:41 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,11 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDvW_HTVOrRzDCz7GwpgqAh8O2uBxziio8",
   authDomain: "sourdough-production-planner.firebaseapp.com",
@@ -9,10 +13,9 @@ const firebaseConfig = {
   storageBucket: "sourdough-production-planner.firebasestorage.app",
   messagingSenderId: "45332994857",
   appId: "1:45332994857:web:7cccb2b85a652899217538",
+  measurementId: "G-V20PFG30Q2"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
